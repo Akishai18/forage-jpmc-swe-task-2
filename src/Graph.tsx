bac5a8f0da40a68dde584ac64e8ec577f0f159a5
Akishai18 <130519154+Akishai18@users.noreply.github.com>
2023-12-02 22:01:53 -0500

    Update Graph.tsx

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -3,35 +3,22 @@ import { Table } from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
 import './Graph.css';
 
-/**
- * Props declaration for <Graph />
- */
 interface IProps {
   data: ServerRespond[],
 }
 
-/**
- * Perspective library adds load to HTMLElement prototype.
- * This interface acts as a wrapper for Typescript compiler.
- */
-interface PerspectiveViewerElement {
-  load: (table: Table) => void,
+interface PerspectiveViewerElement extends HTMLElement {
+  load: (table: Table) => void;
 }
 
-/**
- * React component that renders Perspective based on data
- * parsed from its parent through data property.
- */
 class Graph extends Component<IProps, {}> {
-  // Perspective table
   table: Table | undefined;
 
   render() {
-    return React.createElement('perspective-viewer');
+    return <perspective-viewer />;
   }
 
   componentDidMount() {
-    // Get element to attach the table from the DOM.
     const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
@@ -44,30 +31,33 @@ class Graph extends Component<IProps, {}> {
     if (window.perspective && window.perspective.worker()) {
       this.table = window.perspective.worker().table(schema);
     }
-    if (this.table) {
-      // Load the `table` in the `<perspective-viewer>` DOM reference.
 
-      // Add more Perspective configurations here.
+    if (this.table) {
       elem.load(this.table);
     }
   }
 
   componentDidUpdate() {
-    // Everytime the data props is updated, insert the data into Perspective table
     if (this.table) {
-      // As part of the task, you need to fix the way we update the data props to
-      // avoid inserting duplicated entries into Perspective table again.
-      this.table.update(this.props.data.map((el: any) => {
-        // Format the data from ServerRespond to the schema
-        return {
-          stock: el.stock,
-          top_ask_price: el.top_ask && el.top_ask.price || 0,
-          top_bid_price: el.top_bid && el.top_bid.price || 0,
-          timestamp: el.timestamp,
-        };
-      }));
+      // Update the way we update the data to avoid inserting duplicated entries
+      const uniqueData = Array.from(new Set(this.props.data.map(el => el.timestamp)))
+        .map(timestamp => {
+          const filteredData = this.props.data.filter(el => el.timestamp === timestamp);
+          const averageTopAsk = filteredData.reduce((sum, el) => sum + (el.top_ask?.price || 0), 0) / filteredData.length;
+          const averageTopBid = filteredData.reduce((sum, el) => sum + (el.top_bid?.price || 0), 0) / filteredData.length;
+
+          return {
+            stock: filteredData[0].stock,
+            top_ask_price: averageTopAsk,
+            top_bid_price: averageTopBid,
+            timestamp,
+          };
+        });
+
+      this.table.update(uniqueData);
     }
   }
 }
 
 export default Graph;
+
